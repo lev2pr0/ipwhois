96529fdca036504792153bb503d04c4f79e1c543
Levar Norwood <levar.norwood@outlook.com>
2025-06-19 14:18:30 -0400

    added .ps1 and comment

---
+++ ipwhois.ps1
@@ -0,0 +1,8 @@
+<#
+Filename: ipwhois.ps1
+Author: Levar Norwood
+Date: YYYY-MM-DD
+Version: 1.0
+Description: Briefly describe what your script does.
+README: https://github.com/lev2pr0/ipwhois
+#>
---
+++ ipwhois.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Filename: ipwhois.py
+Author: Levar Norwood
+Date: YYYY-MM-DD
+Version: 1.0
+Description: Briefly describe what your script does.
+README: https://github.com/lev2pr0/ipwhois
+"""
+
+import os
+import sys
